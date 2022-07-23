a2041e29583772e09e91b4397d7c3aefe04bae13
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-07-23 18:55:41 +0300

    Javascript trainning file

---
+++ js/main.js
@@ -0,0 +1,101 @@
+// window.alert("Hello");
+// console.log("log");
+// console.error("error");
+// console.table(["dd","ww","aa"]);
+// var a=2;
+// var a = 3;
+// let a=2;
+// a = 3;
+// console.log(a);
+// var a=2;
+// var a = 3;
+// let a = "We Love";
+// let b = "JavaScript";
+// let c = "And";
+// let d = "Programming";
+// console.log(`${a} ${b} ${c} ${d}`);
+// console.log(10+"alaa");
+// console.log(10-"alaa");
+// console.log(typeof NaN)
+// console.log(20 / 5);
+// console.log(20 / 3);
+// console.log(10 % 2);
+// console.log(11 % 2);
+// let a = "100";
+// let b = 20;
+// let c = true;
+// console.log(a + b + c)
+// console.log(10 - +"")
+// console.log(+false - +true)
+
+
+// let myFriends = ["Ahmed", "Mohamed", "Sayed", ["Marwan", "Ali"]];
+// console.log(`Hello ${myFriends[3][1][2]}`);
+// console.log(myFriends)
+// myFriends[1] = "Gamal";
+// console.log(myFriends);
+// console.log(Array.isArray(myFriends));
+// let myFriends = ["Ahmed", "Mohamed", "Sayed", "Alaa"];
+// console.log(myFriends.length);
+// myFriends.length = 2;
+// console.log(myFriends);
+// let myFriends = ["Ahmed", "Mohamed", "Sayed", "Alaa"];
+
+// console.log(myFriends);
+
+// myFriends.unshift("Osama", "Nabil");
+
+// console.log(myFriends);
+// myFriends.push("Samah", "Eman");
+
+// console.log(myFriends);
+
+// let first = myFriends.shift();
+
+// console.log(myFriends);
+
+// let last = myFriends.pop();
+
+// console.log(myFriends);
+
+// console.log(`Last Name Is ${last}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
