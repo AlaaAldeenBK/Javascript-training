126bd1f37afd86001cb47f7effaa98f437c26ee6
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-07-24 16:41:25 +0300

    Third js file

--- js/main.js
+++ js/main.js
@@ -136,6 +136,39 @@
 //     console.log(`- ${models[k]}`);
 //   }
 // }
+// let products = ["Keyboard", "Mouse", "Pen", "Pad", "Monitor"];
+
+// let colors = ["Red", "Green", "Black"];
+// mainLoop: for (let i = 0; i < products.length; i++) {
+//     console.log(products[i]);
+//     nestedLoop: for (let j = 0; j < colors.length; j++) {
+//       console.log(`- ${colors[j]}`);
+//       if (colors[j] === "Green") {
+//         break mainLoop;
+//       }
+//     }
+//   }
+
+let products = ["Keyboard", "Mouse", "Pen", "Pad", "Monitor", "iPhone"];
+let i = 0;
+
+for (;;) {
+  console.log(products[i]);
+  i++;
+  if (i === products.length) break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
