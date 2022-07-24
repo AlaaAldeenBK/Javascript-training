2b41c67ffa64c6e6552064560b74722102552f49
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-07-24 16:32:50 +0300

    Third js file

--- js/main.js
+++ js/main.js
@@ -102,6 +102,52 @@
 // console.log(allFriends.join(" @ "));
 // console.log(allFriends.join("|"));
 // console.log(allFriends.join("|").toUpperCase());
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+//   }
+// let myFriends = [1, 2, "Osama", "Ahmed", 3, 4, "Sayed", 6, "Ali"];
+
+// let onlyNames = [];
+
+// for (let i = 0; i < myFriends.length; i++) {
+//   if (typeof myFriends[i] === "string") {
+//     onlyNames.push(myFriends[i]);
+//   }
+// }
+// console.log(onlyNames);
+
+
+// let products = ["Keyboard", "Mouse", "Pen", "Pad", "Monitor"];
+
+// let colors = ["Red", "Green", "Black"];
+
+// let models = [2020, 2021];
+
+// for (let i = 0; i < products.length; i++) {
+//   console.log("#".repeat(15));
+//   console.log(`# ${products[i]}`);
+//   console.log("#".repeat(15));
+//   console.log("Colors: ");
+//   for (let j = 0; j < colors.length; j++) {
+//     console.log(`- ${colors[j]}`);
+//   }
+//   console.log("Models: ");
+//   for (let k = 0; k < models.length; k++) {
+//     console.log(`- ${models[k]}`);
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
