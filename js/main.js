0c12be8e00d7df1be1e159389bd22e212d141afe
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-07-25 10:10:07 +0300

    Third js file

--- js/main.js
+++ js/main.js
@@ -329,6 +329,33 @@
 
 // console.log(print(10,20));
 
+// var a = 1;
+// let b = 2;
+
+// function showText() {
+//   var a = 10;
+//   let b = 20;
+//   console.log(`Function - From Local ${a}`);
+//   console.log(`Function - From Local ${b}`);
+// }
+
+// console.log(`From Global ${a}`);
+// console.log(`From Global ${b}`);
+
+// showText();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
