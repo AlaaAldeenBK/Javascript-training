2a1c6f7c3d3ba66a2fddda5c09ec64d5fc23c264
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-07-27 17:24:34 +0300

    Third js File

--- js/main.js
+++ js/main.js
@@ -551,7 +551,13 @@
   
 //   three();
 
-
+// console.log("One");
+// setTimeout(() => console.log("Three"),0);
+// setTimeout(() => console.log("Four"), 0);
+// console.log("Two");
+// setTimeout(() => console.log(myVar), 0);
+// let myVar = 100;
+// myVar += 100;
 
 
 
