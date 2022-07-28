619868119d92db2c93f05c989c24cb9cb9112281
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-07-28 14:16:20 +0300

    Third Js File

--- js/main.js
+++ js/main.js
@@ -562,19 +562,37 @@
 // let req = new XMLHttpRequest();
 // console.log(req);
 
-let myRequest = new XMLHttpRequest();
-myRequest.open("GET","https://api.github.com/users/AlaaAldeenBK/repos", true);
-myRequest.send();
-console.log(myRequest);
-myRequest.onreadystatechange = function()
-{
-    console.log(myRequest.readyState);
-    console.log(myRequest.status);
-    if(this.readyState === 4 && this.status === 200)
-    {
-        console.log(this.responseText)
-    }
-}
+// let myRequest = new XMLHttpRequest();
+// myRequest.open("GET","https://api.github.com/users/AlaaAldeenBK/repos", true);
+// myRequest.send();
+// console.log(myRequest);
+// myRequest.onreadystatechange = function()
+// {
+//     console.log(myRequest.readyState);
+//     console.log(myRequest.status);
+//     if(this.readyState === 4 && this.status === 200)
+//     {
+//         console.log(this.responseText)
+//     }
+// }
+// let myRequest = new XMLHttpRequest();
+// myRequest.open("GET", "https://api.github.com/users/AlaaAldeenBK/repos");
+// myRequest.send();
+// myRequest.onreadystatechange = function()
+// {
+//     if(this.readyState === 4 && this.status === 200)
+//     {
+//         let jsData = JSON.parse(this.responseText);
+//         // console.log(jsData);
+//         for(let i = 0; i<jsData.length; i++)
+//         {
+//             let div = document.createElement("div");
+//             let repoName = document.createTextNode(jsData[i].full_name);
+//             div.appendChild(repoName);
+//             document.body.appendChild(div);
+//         }
+//     }
+// }
 
 
 
