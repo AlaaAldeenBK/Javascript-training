342c27a6e30121657136ade253f0c6014087f02f
AlaaAldeenBK <alshamery.alaaaldeen@gmail.com>
2022-07-28 14:54:33 +0300

    Third js file

--- js/main.js
+++ js/main.js
@@ -594,6 +594,30 @@
 //     }
 // }
 
+// function makeItRed(e) {
+//     e.target.style.color = "red";
+//   }
+//   let p = document.querySelector(".test");
+//   p.addEventListener("click", makeItRed);
+
+//   function iamACallback() {
+//     console.log("Iam A Callback Function");
+//   }
+//   setTimeout(iamACallback, 2000);
+
+// setTimeout(() => {
+//     console.log("Download Photo From URL");
+//     setTimeout(() => {
+//       console.log("Resize Photo");
+//       setTimeout(() => {
+//         console.log("Add Logo To The Photo");
+//         setTimeout(() => {
+//           console.log("Show The Photo In Website");
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+
 
 
 
